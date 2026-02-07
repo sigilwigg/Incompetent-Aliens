adefe66cd4b884c42f69080337e7aa3b9084b40c
fowlie5 <lewis.fowlie@icloud.com>
2026-02-07 16:57:13 +0000

    Added better support for 2 glyphs

--- Assets/Scripts/Environment/Interactables/GlyphPlacing.cs
+++ Assets/Scripts/Environment/Interactables/GlyphPlacing.cs
@@ -8,55 +8,82 @@ using TMPro;
 /*
  * This script is to let the player place the glyph inside the slot
  * 
- * To use this, add the script to any glyph item
- * Set the "Glyph" as the item that you want to pick up
- * Set the "Sarcophagus" as the slot you want to place the item in
+ * To use this, add the script to the base of the Sarcophagus
+ * Set Glyph 1 as the first glyph, and Slot 1 as the place you want it to get inserted
+ * Do the same with Glyph 2 and Slot 2
  */
 
 namespace Glyph
 {
 
     public class GlyphPlacing : MonoBehaviour
-    {   
+    {
         // ----- These will allow the developers to add a specific item of their request to each of the variables -----
-        public GameObject Glyph;
-        public GameObject Slot;
+        public GameObject Glyph_1;
+        public GameObject Slot_1;
+
+        public GameObject Glyph_2;
+        public GameObject Slot_2;
+
+        public bool Glyph_1_inPlace = false;
+        public bool Glyph_2_inPlace = false;
+
+        private Controller m_playerController;
 
         private void Update()
         {
-            CheckDistance();
+            Glyph1();
+            Glyph2();
         }
 
         // ----- Get the distance to the slot -----
-        private void CheckDistance()
+        private void Glyph1()
         {
 
             // ----- These variables determine the range at which you can put the glyph in -----
-            float reach = 2;
+            float reach = 1.5f;
             float distance;
-            
-            // ----- This variable is a boolean that gets updated when the glyph is in place -----
-            bool inPlace;
 
             // ----- This calculates the distance between the glyph and slot, and outputs it as a float -----
-            distance = Vector3.Distance(Glyph.transform.position, Slot.transform.position);
+            distance = Vector3.Distance(Glyph_1.transform.position, Slot_1.transform.position);
 
             if (reach > distance)
             {
-                inPlace = true;
+                Glyph_1_inPlace = true;
 
-                Glyph.transform.position = Slot.transform.position;
+                Glyph_1.transform.position = Slot_1.transform.position;
+
+                // Pickupable itemToDrop = m_playerController.m_currentlyHeldItem.GetComponent<Interactables.Pickupable>();
+
+                // if (itemToDrop != null) itemToDrop.m_collider.enabled = true;
+                // itemToDrop.gameObject.transform.parent = null;
+                // m_playerController.m_currentlyHeldItem = null;
             }
             else
             {
-                inPlace = false;
+                Glyph_1_inPlace = false;
             }
         }
 
+        private void Glyph2()
+        {
+            float reach = 1.5f;
+            float distance;
 
-            
+            distance = Vector3.Distance(Glyph_2.transform.position, Slot_2.transform.position);
+
+            if (reach > distance)
+            {
+                Glyph_2_inPlace = true;
+
+                Glyph_2.transform.position = Slot_2.transform.position;
+            }
+            else
+            {
+                Glyph_2_inPlace = false;
+            }
+        }
 
 
     }
 }
-
