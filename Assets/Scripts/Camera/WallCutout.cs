f0aad5c7c76979633a2aaeaec723533e62399ae3
fowlie5 <lewis.fowlie@icloud.com>
2026-02-25 16:53:54 +0000

    Added counter to register when player is behind wall

--- Assets/Scripts/Camera/WallCutout.cs
+++ Assets/Scripts/Camera/WallCutout.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class WallCutout : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class WallCutout : MonoBehaviour
     private LayerMask wallMask;
 
     private Camera groupCamera;
+
+    private int counter = 0;
+
     private void Awake()
     {
         groupCamera = GetComponent<Camera>();
@@ -17,15 +22,19 @@ public class WallCutout : MonoBehaviour
 
     private void Update()
     {
+
         Vector2 cutoutPos = groupCamera.WorldToViewportPoint(targetObject.position);
 
         cutoutPos.y /= (Screen.width / Screen.height);
+
 
         Vector3 offset = targetObject.position - transform.position;
         RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
 
         for (int i = 0; i < hitObjects.Length; ++i)
         {
+            counter++;
+            Debug.Log(counter);
             Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
 
             for (int m = 0; m < materials.Length; ++m)
