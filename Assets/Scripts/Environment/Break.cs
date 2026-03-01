1c7bd738f9765789b844a094e16e4960dadc21e4
fowlie5 <lewis.fowlie@icloud.com>
2026-03-01 16:03:39 +0000

    Particles play and glyph respawns on contact

--- Assets/Scripts/Environment/Break.cs
+++ Assets/Scripts/Environment/Break.cs
@@ -1,26 +1,30 @@
 
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Break : MonoBehaviour
 {
-    public GameObject particles;
+    [SerializeField]
+    private ParticleSystem particleSystem;
+    public Vector3 m_originalPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        m_originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
         {
-            particles.gameObject.ParticleSystem.Play;
-            Debug.Log("A");
+            particleSystem.Play();
+            transform.position = m_originalPosition;
         }
     }
 }
