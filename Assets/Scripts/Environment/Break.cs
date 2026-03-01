0ace1e85dc7037bd4beb95dd39015fc402c3d40a
fowlie5 <lewis.fowlie@icloud.com>
2026-03-01 14:33:13 +0000

    Started falling glyph and break

---
+++ Assets/Scripts/Environment/Break.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public GameObject particles;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            //ParticleSystem.Play;
+            Debug.Log("A");
+        }
+    }
+}
+
