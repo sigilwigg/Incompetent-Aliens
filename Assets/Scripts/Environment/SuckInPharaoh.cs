882c75589a7b64bcc1e62d5e8d7a571dc79fda0d
Fulgrim <39089386+FulgrimDragon@users.noreply.github.com>
2026-03-25 18:38:23 +0000

    pseudo for suck in script

---
+++ Assets/Scripts/Environment/SuckInPharaoh.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SuckInPharaoh : MonoBehaviour
+{
+    /*
+     * vars
+     *      REFERENCES
+     *          pharaoh ref
+     *          tornado prefab ref
+     *          glyph slot ref
+     *
+     * 
+     * if all glyph slots in place
+     *      disable pharaoh
+     *      enable tornado
+     *      move tornado to sacrophagas
+     *      
+     *      if tornado is in sacrophagas
+     *          disable tornado
+     *          paly close lid anim
+     * 
+     */
+}
