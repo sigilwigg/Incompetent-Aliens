d2f5e4dc00599014b80d84aeb97ec9b55667f4c5
Fulgrim <39089386+FulgrimDragon@users.noreply.github.com>
2026-03-25 18:58:16 +0000

    Update SuckInPharaoh.cs

--- Assets/Scripts/Environment/SuckInPharaoh.cs
+++ Assets/Scripts/Environment/SuckInPharaoh.cs
@@ -2,22 +2,40 @@ using UnityEngine;
 
 public class SuckInPharaoh : MonoBehaviour
 {
-    /*
-     * vars
-     *      REFERENCES
-     *          pharaoh ref
-     *          tornado prefab ref
-     *          glyph slot ref
-     *
-     * 
-     * if all glyph slots in place
-     *      disable pharaoh
-     *      enable tornado
-     *      move tornado to sacrophagas
-     *      
-     *      if tornado is in sacrophagas
-     *          disable tornado
-     *          paly close lid anim
-     * 
-     */
+    public GameObject m_pharaoh; //this must be set to the pharaoh as a whole
+    public Transform m_pharaohTransform; //this must references the 'Enemy' child
+    public GameObject m_tornadoPrefab;
+    public GlyphSlots m_glyphSlots;
+    public bool m_isSarcophagasClosed = false;
+
+    private void Start()
+    {
+        m_glyphSlots = GetComponent<GlyphSlots>();
+    }
+
+    private void Update()
+    {
+        if (!m_isSarcophagasClosed)
+        {
+            HandleSuckInPharaoh();
+        }
+    }
+
+    private void HandleSuckInPharaoh()
+    {
+        if (m_glyphSlots.m_allGlyphSlotsFull == true)
+        {
+            GameObject tornadoGO = Instantiate(m_tornadoPrefab, m_pharaohTransform.position, Quaternion.identity);
+            m_pharaoh.SetActive(false);
+
+            //move tornado to sarc
+
+            if (tornadoGO.transform.position == transform.position)
+            {
+                Destroy(tornadoGO);
+                //play closing lid animation
+                m_isSarcophagasClosed = true;
+            }
+        }
+    }
 }
