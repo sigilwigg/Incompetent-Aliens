9301e35c75d61e5ae688c24ebf70db8a84f80b6d
Lewis <lewis.fowlie@icloud.com>
2026-04-15 13:37:11 +0100

    Particle system can now be called through mirror's picked up state

--- Assets/Scripts/Environment/Interactables/MirrorVFX.cs
+++ Assets/Scripts/Environment/Interactables/MirrorVFX.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using Interactables;
 
 public class MirrorVFX : MonoBehaviour
 {
+    public ParticleSystem particleSystem;
+    public GameObject mirror;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,18 @@ public class MirrorVFX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPickup();
+    }
+
+    private void CheckPickup()
+    {
+        if (mirror.GetComponent<StackPickupable>().m_isPickedUp)
+        {
+            PlayParticles();
+        }
+    }
+    private void PlayParticles()
+    {
+        particleSystem.Play();
     }
 }
