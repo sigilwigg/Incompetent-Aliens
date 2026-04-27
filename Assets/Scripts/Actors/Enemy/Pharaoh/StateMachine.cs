b0e4a5e29fdb1a5c2fe8cc2f52bee12a4039e30c
Fulgrim <39089386+FulgrimDragon@users.noreply.github.com>
2026-04-27 16:46:35 +0100

    all player get thrown now

--- Assets/Scripts/Actors/Enemy/Pharaoh/StateMachine.cs
+++ Assets/Scripts/Actors/Enemy/Pharaoh/StateMachine.cs
@@ -61,7 +61,7 @@ namespace Enemy.Pharaoh
             //----- check if player is caught -----
             if (m_pharaohBlackboard.m_canCatchPlayer && !m_actions.m_isThrowing)
             {
-                StartCoroutine(m_actions.ThrowPlayerCoroutine(m_throwCooldown, m_throwForce));
+                m_actions.ThrowPlayers(m_throwCooldown, m_throwForce);
             }
 
             //----- decide state -----
